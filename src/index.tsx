346d18481d65617804f7c70bce38f83d13d12235
Quien <quien697@gmail.com>
2024-03-05 16:44:22 -0800

    refactor: use a div to wrap app

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+const root = document.createElement("div");
+root.id = "chrome-extension-popup";
+document.body.appendChild(root);
+
+const rootDiv = ReactDOM.createRoot(root);
+rootDiv.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
